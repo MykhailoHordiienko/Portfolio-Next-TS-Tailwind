d5b8d3bed789dff3105a8f863a7bbaf68daae4b3
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-03-06 12:35:51 +0200

    Start

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Next</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      
+        <h1 className='text-red-500' >Hi sdkjfhsdkhfkjsdhfkhsdkjfkjsdhfkjsd</h1>
+     
+    </div>
+  )
+}
+
+export default Home
