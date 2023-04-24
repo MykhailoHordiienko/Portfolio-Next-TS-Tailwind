00c6dee69046295ee7de80aa08ee7ec4a987abe5
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-04-24 18:38:50 +0300

    add 2 projects

--- helpers/projects.ts
+++ helpers/projects.ts
@@ -5,8 +5,26 @@ import proj4 from "../public/project-photo/proj-4.jpg";
 import proj5 from "../public/project-photo/proj-5.jpg";
 import news from "../public/project-photo/news-proj.jpg";
 import todo from "../public/project-photo/todo-proj.jpg";
+import soyummy from "../public/project-photo/soyummy.jpg";
+import reactNative from "../public/project-photo/react-native.jpg";
 
 export const projects = [
+  {
+    photo: soyummy,
+    title: "SoYummy",
+    description:
+      "FullStack App, your own digital cookbook. Using [Rect, Axios, Tailwind CSS, Redux Toolkit, Formik, Motion]",
+    git: "https://github.com/MykhailoHordiienko/SoYammyFront",
+    live: "http://vladyslavhurzhyi.github.io/soyummy/",
+  },
+  {
+    photo: reactNative,
+    title: "ReactNative",
+    description:
+      "Application, with photos, geolocation, comments and your own posts. Using [React Native / Navigation, Expo, Firebase, Redux Toolkit]",
+    git: "https://github.com/MykhailoHordiienko/rect-native",
+    live: "https://expo.dev/@mykhailohordiienko/reatNative?serviceType=classic&distribution=expo-go",
+  },
   {
     photo: proj1,
     title: "Phone Book",
