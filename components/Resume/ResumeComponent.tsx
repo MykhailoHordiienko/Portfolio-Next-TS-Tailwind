ad063e75626a598ab1fabeb2629f682188d22578
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-10-16 16:12:50 +0300

    change CV

--- components/Resume/ResumeComponent.tsx
+++ components/Resume/ResumeComponent.tsx
@@ -1,13 +1,13 @@
-import { motion } from "framer-motion";
-import CV from "../../public/CV.png";
-import React from "react";
-import useDownloader from "react-use-downloader";
-import Image from "next/image";
+import { motion } from 'framer-motion';
+import CV from '../../public/CV.png';
+import React from 'react';
+import useDownloader from 'react-use-downloader';
+import Image from 'next/image';
 
 const ResumeComponent = () => {
   const { download } = useDownloader();
-  const fileDir = "/Mykhailo_Hordiienko_Front-End_Developer.pdf";
-  const fileName = "Mykhailo_Hordiienko_Front-End_Developer.pdf";
+  const fileDir = '/Mykhailo-Hordiienko-Full-Stack.pdf';
+  const fileName = 'Mykhailo-Hordiienko-Full-Stack.pdf';
   return (
     <motion.div
       className="flex flex-col gap-10"
@@ -15,11 +15,7 @@ const ResumeComponent = () => {
       transition={{ duration: 1.5 }}
       whileInView={{ opacity: 1 }}
       viewport={{ once: true }}>
-      <Image
-        className="w-[250px] md:w-[400px]"
-        src={CV}
-        alt="CV"
-      />
+      <Image className="w-[250px] md:w-[400px]" src={CV} alt="CV" />
       <button
         onClick={() => download(fileDir, fileName)}
         className="contact-btn">
--- helpers/projects.ts
+++ helpers/projects.ts
@@ -1,84 +1,84 @@
-import proj1 from "../public/project-photo/proj-1.jpg";
-import proj2 from "../public/project-photo/proj-2.jpg";
-import proj3 from "../public/project-photo/proj-3.jpg";
-import proj4 from "../public/project-photo/proj-4.jpg";
-import proj5 from "../public/project-photo/proj-5.jpg";
-import news from "../public/project-photo/news-proj.jpg";
-import todo from "../public/project-photo/todo-proj.jpg";
-import soyummy from "../public/project-photo/soyummy.jpg";
-import reactNative from "../public/project-photo/react-native.jpg";
+import proj1 from '../public/project-photo/proj-1.jpg';
+import proj2 from '../public/project-photo/proj-2.jpg';
+import proj3 from '../public/project-photo/proj-3.jpg';
+import proj4 from '../public/project-photo/proj-4.jpg';
+import proj5 from '../public/project-photo/proj-5.jpg';
+import news from '../public/project-photo/news-proj.jpg';
+import todo from '../public/project-photo/todo-proj.jpg';
+import soyummy from '../public/project-photo/soyummy.jpg';
+import reactNative from '../public/project-photo/react-native.jpg';
 
 export const projects = [
   {
     photo: soyummy,
-    title: "SoYummy",
+    title: 'SoYummy',
     description:
-      "FullStack App, your own digital cookbook. Using [Rect, Axios, Tailwind CSS, Redux Toolkit, Formik, Motion]",
-    git: "https://github.com/MykhailoHordiienko/SoYammyFront",
-    live: "http://vladyslavhurzhyi.github.io/soyummy/",
+      'FullStack App, your own digital cookbook. Using [Rect, Axios, Tailwind CSS, Redux Toolkit, Formik, Motion]',
+    git: 'https://github.com/MykhailoHordiienko/SoYammyFront',
+    live: 'http://vladyslavhurzhyi.github.io/soyummy/',
   },
   {
     photo: reactNative,
-    title: "ReactNative",
+    title: 'ReactNative',
     description:
-      "Application, with photos, geolocation, comments and your own posts. Using [React Native / Navigation, Expo, Firebase, Redux Toolkit]",
-    git: "https://github.com/MykhailoHordiienko/rect-native",
-    live: "https://expo.dev/@mykhailohordiienko/reatNative?serviceType=classic&distribution=expo-go",
+      'Application, with photos, geolocation, comments and your own posts. Using [React Native / Navigation, Expo, Firebase, Redux Toolkit]',
+    git: 'https://github.com/MykhailoHordiienko/rect-native',
+    live: 'https://expo.dev/@mykhailohordiienko/reatNative?serviceType=classic&distribution=expo-go',
   },
   {
     photo: proj1,
-    title: "Phone Book",
+    title: 'Phone Book',
     description:
-      "Application, your personal phone book with authorization and login. App component with Public and Private routes, AuthPage page logic. Using [React, Redux, Axios, Styled Components, Webpack, Chakra-Ui]",
-    git: "https://github.com/MykhailoHordiienko/goit-react-hw-08-phonebook",
-    live: "https://mykhailohordiienko.github.io/goit-react-hw-08-phonebook/",
+      'Application, your personal phone book with authorization and login. App component with Public and Private routes, AuthPage page logic. Using [React, Redux, Axios, Styled Components, Webpack, Chakra-Ui]',
+    git: 'https://github.com/MykhailoHordiienko/goit-react-hw-08-phonebook',
+    live: 'https://mykhailohordiienko.github.io/goit-react-hw-08-phonebook/',
   },
   {
     photo: todo,
-    title: "To Do List",
+    title: 'To Do List',
     description:
-      "Application, your personal simple ToDo list. Using [React, Axios, TS, React-Query, mockApi, Tailwind CSS]",
-    git: "https://github.com/MykhailoHordiienko/todolist-ts-react-query-tailwind",
-    live: "https://mykhailohordiienko.github.io/todolist-ts-react-query-tailwind/",
+      'Application, your personal simple ToDo list. Using [React, Axios, TS, React-Query, mockApi, Tailwind CSS]',
+    git: 'https://github.com/MykhailoHordiienko/todolist-ts-react-query-tailwind',
+    live: 'https://mykhailohordiienko.github.io/todolist-ts-react-query-tailwind/',
   },
   {
     photo: news,
-    title: "News App",
+    title: 'News App',
     description:
-      "Application, Live News & Blog Articles. Using [Next.js 13, Axios, Tailwind CSS, TS, mediastackAPI]",
-    git: "https://github.com/MykhailoHordiienko/news-next-ts-tailwind",
-    live: "https://news-next-ts-tailwind.vercel.app//",
+      'Application, Live News & Blog Articles. Using [Next.js 13, Axios, Tailwind CSS, TS, mediastackAPI]',
+    git: 'https://github.com/MykhailoHordiienko/news-next-ts-tailwind',
+    live: 'https://news-next-ts-tailwind.vercel.app/',
   },
   {
     photo: proj2,
-    title: "Movies Search",
+    title: 'Movies Search',
     description:
-      "Movie search app. [React, React-Router, Axios, Styled Components, Webpack]",
-    git: "https://github.com/MykhailoHordiienko/goit-react-hw-05-movies",
-    live: "https://mykhailohordiienko.github.io/goit-react-hw-05-movies/",
+      'Movie search app. [React, React-Router, Axios, Styled Components, Webpack]',
+    git: 'https://github.com/MykhailoHordiienko/goit-react-hw-05-movies',
+    live: 'https://mykhailohordiienko.github.io/goit-react-hw-05-movies/',
   },
   {
     photo: proj3,
-    title: "Ice-Cream",
+    title: 'Ice-Cream',
     description:
-      "Ice Cream company site. Neat and tasty design, responsible layout. Using [Parcel, HTML, SCSS, JS]",
-    git: "https://github.com/MykhailoHordiienko/Ice-Cream",
-    live: "https://mykhailohordiienko.github.io/Ice-Cream/",
+      'Ice Cream company site. Neat and tasty design, responsible layout. Using [Parcel, HTML, SCSS, JS]',
+    git: 'https://github.com/MykhailoHordiienko/Ice-Cream',
+    live: 'https://mykhailohordiienko.github.io/Ice-Cream/',
   },
   {
     photo: proj4,
-    title: "Filmoteka",
+    title: 'Filmoteka',
     description:
-      "Application for choosing a movie. Role: Team Lead. App component with Public and Private routes, AuthPage page logic. Using [HTML, SASS, JS, REST API, AJAX, Parcel]",
-    git: "https://github.com/MykhailoHordiienko/Filmoteka",
-    live: "https://mykhailohordiienko.github.io/Filmoteka/",
+      'Application for choosing a movie. Role: Team Lead. App component with Public and Private routes, AuthPage page logic. Using [HTML, SASS, JS, REST API, AJAX, Parcel]',
+    git: 'https://github.com/MykhailoHordiienko/Filmoteka',
+    live: 'https://mykhailohordiienko.github.io/Filmoteka/',
   },
   {
     photo: proj5,
-    title: "Web-Studio",
+    title: 'Web-Studio',
     description:
-      "Web Studio company site. Project width responsible layout and CSS animation. Using [HTML, SCSS, JS]",
-    git: "https://github.com/MykhailoHordiienko/goit-markup-hw-08",
-    live: "https://mykhailohordiienko.github.io/goit-markup-hw-08/index.html",
+      'Web Studio company site. Project width responsible layout and CSS animation. Using [HTML, SCSS, JS]',
+    git: 'https://github.com/MykhailoHordiienko/goit-markup-hw-08',
+    live: 'https://mykhailohordiienko.github.io/goit-markup-hw-08/index.html',
   },
 ];
